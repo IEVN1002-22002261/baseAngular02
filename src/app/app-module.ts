066a8ece72bbd7dbf75419580ba65ab9da5577e2
JunTres <utljun3@gmail.com>
2025-09-13 10:07:05 -0600

    first commit

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { HeroesList } from './heroes/heroes-list/heroes-list';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    HeroesList
   ],
   imports: [
     BrowserModule,
--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,17 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
+  tittle = 'baseANgular02';
+
+  duplicarNumero(numero: number): number{
+    return numero * 2;
+  }
+
+  pelicula ={
+    titulo: 'Batman',
+    anio: new Date(),
+    precio: 2500,
+  }
+
   protected readonly title = signal('baseAngular02');
 }
